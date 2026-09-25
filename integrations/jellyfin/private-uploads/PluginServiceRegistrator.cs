246ce92b0da60d198115b088abb62936afe9dd4e
kingrabbit89 <kingrabbit89@gmail.com>
2026-09-25 13:21:18 +0200

    Add private Jellyfin forum uploads service

---
+++ integrations/jellyfin/private-uploads/PluginServiceRegistrator.cs
@@ -0,0 +1,13 @@
+using MediaBrowser.Controller;
+using MediaBrowser.Controller.Plugins;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jellyfin.Plugin.SelectionTvPrivate;
+
+public sealed class PluginServiceRegistrator : IPluginServiceRegistrator
+{
+    public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
+    {
+        serviceCollection.AddSingleton<ForumUploadsService>();
+    }
+}
